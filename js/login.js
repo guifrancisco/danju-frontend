7e0c2a8849ae34b62caf10f5e3aa2e334bdb6c5b
guifrancisco <guifrancisco.almeida@outlook.com>
2023-10-16 19:10:33 -0300

    feat: :tada: kickstarting danju-frontend

---
+++ js/login.js
@@ -0,0 +1,64 @@
+//TODO : Its a Completed Code
+function formvalid() {
+    var vaildpass = document.getElementById("pass").value;
+  
+    if (vaildpass.length <= 8 || vaildpass.length >= 20) {
+      document.getElementById("vaild-pass").innerHTML = "Mínimo de 8 caracteres";
+      return false;
+    }
+    else {
+      document.getElementById("vaild-pass").innerHTML = "";
+    }
+  }
+
+function show() {
+    var x = document.getElementById("pass");
+    var showimg = document.getElementById("showimg");
+    if (x.type === "password") {
+      x.type = "text";
+      // Exemplo de SVG para 'mostrar'
+      showimg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><circle cx="256" cy="256" r="64"/><path d="M490.84 238.6c-26.46-40.92-60.79-75.68-99.27-100.53C349 110.55 302 96 255.66 96c-42.52 0-84.33 12.15-124.27 36.11-40.73 24.43-77.63 60.12-109.68 106.07a31.92 31.92 0 00-.64 35.54c26.41 41.33 60.4 76.14 98.28 100.65C162 402 207.9 416 255.66 416c46.71 0 93.81-14.43 136.2-41.72 38.46-24.77 72.72-59.66 99.08-100.92a32.2 32.2 0 00-.1-34.76zM256 352a96 96 0 1196-96 96.11 96.11 0 01-96 96z"/></svg>`;
+    } else {
+      x.type = "password";
+      // Exemplo de SVG para 'ocultar'
+      showimg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M432 448a15.92 15.92 0 01-11.31-4.69l-352-352a16 16 0 0122.62-22.62l352 352A16 16 0 01432 448zM248 315.85l-51.79-51.79a2 2 0 00-3.39 1.69 64.11 64.11 0 0053.49 53.49 2 2 0 001.69-3.39zM264 196.15L315.87 248a2 2 0 003.4-1.69 64.13 64.13 0 00-53.55-53.55 2 2 0 00-1.72 3.39z"/><path d="M491 273.36a32.2 32.2 0 00-.1-34.76c-26.46-40.92-60.79-75.68-99.27-100.53C349 110.55 302 96 255.68 96a226.54 226.54 0 00-71.82 11.79 4 4 0 00-1.56 6.63l47.24 47.24a4 4 0 003.82 1.05 96 96 0 01116 116 4 4 0 001.05 3.81l67.95 68a4 4 0 005.4.24 343.81 343.81 0 0067.24-77.4zM256 352a96 96 0 01-93.3-118.63 4 4 0 00-1.05-3.81l-66.84-66.87a4 4 0 00-5.41-.23c-24.39 20.81-47 46.13-67.67 75.72a31.92 31.92 0 00-.64 35.54c26.41 41.33 60.39 76.14 98.28 100.65C162.06 402 207.92 416 255.68 416a238.22 238.22 0 0072.64-11.55 4 4 0 001.61-6.64l-47.47-47.46a4 4 0 00-3.81-1.05A96 96 0 01256 352z"/></svg>`;
+    }
+}  
+
+async function onLogin(url = "http://localhost:8080/v1/auth/login") {
+
+  var username = document.getElementById("user").value;
+  var password = document.getElementById("pass").value;
+  var data = {username, password}
+
+  const response = await fetch(url, {
+    method: "POST", 
+    mode: "cors", 
+    cache: "no-cache", 
+    credentials: "same-origin", 
+    headers: {
+      "Content-Type": "application/json",
+      
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer", 
+    body: JSON.stringify(data), 
+  });
+  return response.json(); 
+}
+
+
+// Inicializar com o SVG "ocultar"
+document.addEventListener("DOMContentLoaded", function() {
+    
+    document.getElementById("showimg").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M432 448a15.92 15.92 0 01-11.31-4.69l-352-352a16 16 0 0122.62-22.62l352 352A16 16 0 01432 448zM248 315.85l-51.79-51.79a2 2 0 00-3.39 1.69 64.11 64.11 0 0053.49 53.49 2 2 0 001.69-3.39zM264 196.15L315.87 248a2 2 0 003.4-1.69 64.13 64.13 0 00-53.55-53.55 2 2 0 00-1.72 3.39z"/><path d="M491 273.36a32.2 32.2 0 00-.1-34.76c-26.46-40.92-60.79-75.68-99.27-100.53C349 110.55 302 96 255.68 96a226.54 226.54 0 00-71.82 11.79 4 4 0 00-1.56 6.63l47.24 47.24a4 4 0 003.82 1.05 96 96 0 01116 116 4 4 0 001.05 3.81l67.95 68a4 4 0 005.4.24 343.81 343.81 0 0067.24-77.4zM256 352a96 96 0 01-93.3-118.63 4 4 0 00-1.05-3.81l-66.84-66.87a4 4 0 00-5.41-.23c-24.39 20.81-47 46.13-67.67 75.72a31.92 31.92 0 00-.64 35.54c26.41 41.33 60.39 76.14 98.28 100.65C162.06 402 207.92 416 255.68 416a238.22 238.22 0 0072.64-11.55 4 4 0 001.61-6.64l-47.47-47.46a4 4 0 00-3.81-1.05A96 96 0 01256 352z"/></svg>`;
+});
+
+// Inicializar o envio de dados para o servidor
+document.addEventListener("DOMContentLoaded", function() {
+    var submitButton = document.getElementById("submitButton");
+    
+    submitButton.addEventListener("click", function() {
+        onLogin();
+    });
+});
